81418a18b5df3e148c1be6276de067ae5d529980
roman <zhrovl@gmail.com>
2014-11-27 00:11:46 +0300

    change login of main page animations

--- js/main_page.js
+++ js/main_page.js
@@ -2,10 +2,11 @@ var main_logic = {
     animateInAll: function(){
         //alert(1)
         var duration = 300;
-        $('#left-menu').css("background-color", "transparent");
-        $('#left-menu').css("background-image", "url('css/images/main-menu-bg.png')");
-        $('#logo').css("background-image", "none");
-        $('body').css("background-image", "/css/images/main_bg.png");
+
+
+        $('#logo').addClass('hide_image')
+        $('body').addClass('bg_body');
+        $('#left-menu').addClass('transparent-menu')
 
         $('#float_background').hide()
         $('#main-logo').animate({
@@ -19,16 +20,13 @@ var main_logic = {
             duration: duration, queue: false
         })
         $('#main_page').css('background-color', 'transparent');
-        //$('body').css('background-repeat', 'no-repeat');
-        //$('body').css('background-image', 'url("css/images/main_bg.png")');
+
 
     },
     animateOutAll: function(){
         var duration = 300;
 
-        $('#left-menu').css("background-color", "");
-        $('#left-menu').css("background-image", "");
-        $('#logo').css("background-image", "url('css/images/logo.png')");
+
 
         $('#float_background').show()
         $('#main-logo').animate({
@@ -41,6 +39,9 @@ var main_logic = {
         }, {
             duration: duration, queue: false
         })
+        $('#logo').removeClass('hide_image')
+        $('body').removeClass('bg_body');
+        $('#left-menu').removeClass('transparent-menu')
     } ,
     fillData: function(){
         $('#salary_avg').text(thousands_sep(avg_salary[avg_salary.length-1])+ '₽')
@@ -54,12 +55,12 @@ var main_logic = {
             payed+=obj.payed  + obj.avans_pogasheno + obj.avans_ne_pogasheno
             payed_left+=obj.payed_left
             work_compl[0]+= obj.work_complete
-            work_compl[1]+= obj.work_left
+            work_compl[1]+= obj.work_left+obj.work_left
         });
 
-        $('#payed_status').text(thousands_sep((payed/1000000).toFixed(0))+'/'+thousands_sep((payed_left/1000000).toFixed(0))+ 'млн ₽')
+        $('#payed_status').text(thousands_sep((payed/1000000).toFixed(0))+'/'+thousands_sep((payed_left/1000000).toFixed(0))+ ' млн ₽')
         $('#active_obj_count').text(data.objects.length)
-        $('#work_percent').text((work_compl[0]/(work_compl[0]+work_compl[1])).toFixed(2)+'%')
+        $('#work_percent').text((work_compl[0]*100/(work_compl[1])).toFixed(2)+'%')
 
         $('#org_count').text(organizations.length)
 
@@ -69,25 +70,22 @@ var main_logic = {
         var one_qty = 0;
 
         var year = new Date().getFullYear();
-
-
-
         $.each(tenders, function(i,val ){
             if(val.year_finish!=year) return
             summ+= val.price_end;
             count++;
             percent+=val.percent
-            if (val.qty_accept==1) one_qty+= val.price_end
+            if (val.bid_accept==1) one_qty+= val.price_end
         })
 
-        $('#summ_count_tenders').text(thousands_sep((summ/1000000000).toFixed(0))+'млрд ₽ / '+count )
-        $('#tenders_with_one_qty').text((one_qty/summ).toFixed(2)+'%')
+        $('#summ_count_tenders').text(thousands_sep((summ/1000000000).toFixed(0))+' млрд ₽ / '+count )
+        $('#tenders_with_one_qty').text((one_qty*100/summ).toFixed(2)+'%')
         $('#tenders_percent').text((percent/count).toFixed(2)+'%')
 
         var apart_count=0
         var m2_price =0
         $.each(apartments, function(i,val ){
-           if(val.status=='ПС'||val.status=='ДКП') {
+           if(val.status=='ПС') {
                 apart_count++;
                 m2_price+=val.price_m2_end
            }
